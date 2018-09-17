a8c10525a4f3d69923a3c7bb56517c19707902ce
jxj322991 <228322991@qq.com>
2018-09-17 21:17:32 +0800

    fix

--- xcx/src/components/goodsInfoNav.vue
+++ xcx/src/components/goodsInfoNav.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="goods" >
-    <img :src="goods.url" alt="goods.url">
-    <div class="listBox">
-    <h3 class="first_c">{{goods.title}}</h3>
-    <h5 class="second_c "><span class="bg_c">&nbsp;版本{{goods.verison}}&nbsp;</span></h5>
-    <p class="theme_c">￥{{goods.money*goods.time}}</p>
-    <P class="fist_c"><span class="theme_c">￥{{goods.money}}</span>×{{goods.time}}期</P>
-    <div class="btn" v-if="goods.free"><span class="bg_c theme_c">&nbsp;免息&nbsp;</span></div>
+  <div class="nav">
+    <div class="line_c h1">
+
+    </div>
+    <div class="box">
+      <span class="list1 first_c">月供
+        <span class="theme_c">￥{{goods.money}}</span>
+      </span>
+      <span class="list3"></span>
+      <span class="list2 theme_bg_c">立即购买</span>
     </div>
   </div>
 </template>
@@ -17,55 +19,41 @@ export default {
   data() {
     return {};
   },
-  methods: {
-   
-  }
+  methods: {}
 };
 </script>
 
 <style scoped lang='scss'>
-.goods { 
-  height: 150rpx;
-  padding: 10rpx;
-  position: relative;
- 
-  img {
-    width: 150rpx;
-    height: 150rpx;
-    box-sizing: border-box;
-    border: 1rpx solid #ededf0;
-  }
-  .listBox{
-    width: 75vw;
-    height: 170rpx;
-    padding: 10rpx 0;
-    box-sizing: border-box;
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
-  p {
-    font-size: 20rpx;
-    line-height: 1.5;
-    
-  }
-  h3{
-    margin-top: 5rpx;
-    font-size: 30rpx;
-    line-height: 1.5;
-  }
-  h5{
-    font-size: 25rpx;
-    line-height: 1.5;
-  }
-  .btn{
-    position: absolute;
-    bottom:  50rpx;
-    right: 50rpx;
-    span{
-      line-height: 1.5;
-      font-size: 25rpx;
+.nav {
+  position: fixed;
+  bottom: 0;
+  height: 90rpx;
+  width: 100vw;
+  background: #fff;
+  .box {
+    display: flex;
+    justify-content: space-between;
+    span {
+      line-height: 90rpx;
     }
+    .list1 {
+      margin-left: 30rpx;
+      padding:0 30rpx 0 0;
+      border-right: 1rpx solid #ededf0;
+      font-size: 30rpx;
+    }
+    .list2{
+      color: #fff;
+      padding: 0 50rpx ;
+    }
+    .list3{
+      padding: 0 50rpx;
+      background: url(../../static/img/service.png) no-repeat center;
+      background-size: 50rpx;
+    }
+  }
+  .h1 {
+    height: 1rpx;
   }
 }
 </style>
--- xcx/src/pages/goodsInfo/index.vue
+++ xcx/src/pages/goodsInfo/index.vue
@@ -11,12 +11,45 @@
       </swiper>
     </div>
     <goods-info-head :goods='goodsInfo'></goods-info-head>
+    <div v-if="goodsInfo.free==1">
+      <div class="bg_c h10"></div>
+      <div class="listbox">
+        <span class="second_c">促销</span>
+        <span class="theme_c">免息</span>
+      </div>
+    </div>
+
     <div class="bg_c h10"></div>
+    <div class="listbox option">
+      <span class="second_c ">已选</span>
+      <span class="second_c">版本 {{goodsInfo.verison}}</span>
+    </div>
+    <div class="bg_c h1"></div>
+
+    <div class="listbox option">
+      <span class="second_c">送至</span>
+      <span class="second_c">你的地址</span>
+    </div>
+
+    <div class="bg_c h10"></div>
+    <div class="listbox option">
+      <span class="second_c">规格参数/包装售后</span>
+    </div>
+
+    <div class="bg_c h10"></div>
+    <div class="listbox">
+      <div class="richText">
+      <rich-text :nodes="page" ></rich-text>
+      </div>
+    </div>
+
+    <div class="bg_c h100"></div>
+
+    <goods-info-nav :goods='goodsInfo'></goods-info-nav>
   </div>
 </template>
 
 <script>
-import goodsInfoBody from "@/components/goodsInfoBody";
 import goodsInfoHead from "@/components/goodsInfoHead";
 import goodsInfoNav from "@/components/goodsInfoNav";
 
@@ -33,12 +66,13 @@ export default {
         "https://i8.mifile.cn/v1/a1/b523499a-ec22-37fc-8165-a1f5eae1e82c.webp",
         "https://i8.mifile.cn/v1/a1/a64944d5-3213-66f0-395d-c11229cfc6f1.webp"
       ],
-      goodsInfo: {}
+      goodsInfo: {},
+      page:' '
     };
   },
 
   components: {
-    goodsInfoBody,
+    
     goodsInfoHead,
     goodsInfoNav
   },
@@ -47,13 +81,12 @@ export default {
     getList() {
       this.goodsInfo = wx.getStorageSync("goodsInfo");
       var that = this;
-      return;
       wx.request({
         url:
-          "https://easy-mock.com/mock/5b9b68d11615c53f58edfeda/getGoodsClass",
+          "https://easy-mock.com/mock/5b1b95e6e4e8507ccf4dcd14/06/jxj/getProductInfo",
         methods: "get",
         success: res => {
-          if (res.data.code == 1) {
+          if (res.data.code == 0) {
             that.upList(res.data.data);
           } else {
             console.log("网络错误!");
@@ -61,19 +94,18 @@ export default {
         }
       });
     },
-    upList(data) {}
+    upList(data) {
+      console.log(data.result.introduction)
+      this.page=data.result.introduction;
+    }
   },
 
   created() {},
   onShow() {
     this.getList();
   },
-  onHide() {
-    this.listData.goodsList = [];
-  },
+  onHide() {},
   onLoad: function() {
-    this.listData.goodsList = [];
-
     wx.setNavigationBarTitle({
       title: "商品详情"
     });
@@ -106,8 +138,35 @@ export default {
       }
     }
   }
-  .h10{
+  .listbox {
+    font-size: 30rpx;
+    line-height: 2;
+    padding: 20rpx 60rpx 20rpx 20rpx;
+    display: flex;
+    justify-content: space-between;
+
+    &.option {
+      background: url(../../../static/img/arrow.png) no-repeat 95vw center;
+      background-size: 20rpx 20rpx;
+    }
+    span {
+    }
+  }
+  .richText{
+    width: 100vw;
+    zoom: 0.47;
+    img{
+      width: 100%;
+    }
+  }
+  .h100{
+    height: 100rpx;
+  }
+  .h10 {
     height: 10rpx;
+  }
+  .h1 {
+    height: 1rpx;
   }
 }
 </style>
